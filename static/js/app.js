f6c3077a073563c66e305457e2803c0f5c6090b3
Elmar Angao <32320010+eangao@users.noreply.github.com>
2023-08-17 13:56:51 +0800

    commit #1
    upload django project to github

---
+++ static/js/app.js
@@ -0,0 +1,34 @@
+
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+{
+    message_timeout.style.display = "none";
+
+}, 5000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
